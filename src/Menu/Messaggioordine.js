5f736cd25b9874e687b55a74f11f4984766141c5
giorgiocoppola <giorgiocoppola12@gmail.com>
2022-01-27 20:43:30 +0100

    Nuova pagina di avviso

---
+++ src/Menu/Messaggioordine.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Messaggioordine.css'
+
+function Messaggioordine() {
+  return <div>
+      <h1 className='messord'>Grazie per aver ordinato le pizze <br /> ricevera le pizze al piu' presto</h1>
+  </div>
+}
+
+export default Messaggioordine;
