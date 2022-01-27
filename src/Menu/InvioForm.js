5923b2cc09f8b7956709eb6df49213fb3b2df9a8
giorgiocoppola <giorgiocoppola12@gmail.com>
2022-01-27 21:08:09 +0100

    Inserimento messaggio del form inviato

---
+++ src/Menu/InvioForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './InvioForm.css'
+
+function InvioForm() {
+  return <div>
+      <h1 className="invioform">Congratulazioni!<br />Hai appena inviato i dati con successo</h1>
+  </div>;
+}
+
+export default InvioForm;
